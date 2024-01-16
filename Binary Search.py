1387d3d44133d7cd9b6c7719ab622f36ae54bdde
Tom <thomasjh@gmail.com>
2024-01-16 16:54:45 +0100

    slightly changed logic, complete driver and output

--- Binary Search.py
+++ Binary Search.py
@@ -7,22 +7,55 @@ def main():
 
     for i in range(random.randint(0,13)):
         worklist.append(random.randint(0,13))
+    
+    if not worklist:
+        quit()
+
+    worklist.sort()
 
     span = len(worklist) - 1
     if span == 0:
-        return 0
-    random_index = worklist[random.randint(0,span)]
+        exit()
 
-    print(f'looking for {random_index} in worklist')
+    lowest_value = worklist[0]
+
+    print(f"Randomly Generated List: {worklist}")
+    print("-----")
+  
+    for i in range(random.randint(0,span)):
+        value = worklist.pop(0)
+        worklist.append(value)    
+    print(f"List is rotated, moving one spot each time, randomly: {worklist}")
+    print("------")
+   
+    print(f'looking for {lowest_value} in worklist:')
+    print("------")
+
+    ###Find the first instance of the lowest number in sorted list
 
     low_number = 0
     high_number = span
+    found = False
 
-    while high_number > low_number:
+    while found == False:
         middle = (low_number + high_number) // 2
         middle_value = worklist[middle]
-        print(worklist)
-        print(middle_value)
+        low_value = worklist[0]
+        high_value = worklist[span]
+
+        if low_value <= high_value:
+            print("Array has not been rotated, position 0 is answer.")
+            found = True
+
+        elif middle > 0 and middle_value < worklist[middle - 1]:
+            print(f"The answer is {middle_value} at position {middle}")
+            found = True
+
+        elif middle_value > worklist[middle + 1]:
+            low_number = middle + 1
+
+        else:
+            high_number = middle - 1
 
 
 if __name__ == '__main__':
