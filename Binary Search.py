8cc5516b735d06871f29a3f2dfe794a11846c06d
Tom <thomasjh@gmail.com>
2024-01-16 14:22:53 +0100

    slight changes to logic

--- Binary Search.py
+++ Binary Search.py
@@ -8,12 +8,21 @@ def main():
     for i in range(random.randint(0,13)):
         worklist.append(random.randint(0,13))
 
-    span = len(worklist)
+    span = len(worklist) - 1
     if span == 0:
         return 0
     random_index = worklist[random.randint(0,span)]
 
     print(f'looking for {random_index} in worklist')
+
+    low_number = 0
+    high_number = span
+
+    while high_number > low_number:
+        middle = (low_number + high_number) // 2
+        middle_value = worklist[middle]
+        print(worklist)
+        print(middle_value)
 
 
 if __name__ == '__main__':
