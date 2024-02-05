6d1aad0392fb1958e5cf5709bf52dbaaea674e17
Tom <thomasjh@gmail.com>
2024-02-05 13:36:11 +0100

    word count in a dict format

--- Hash Table Exercises.py
+++ Hash Table Exercises.py
@@ -41,3 +41,44 @@ with open("nyc_weather.csv", "r") as f:
 print(temperatures)
 print(temperatures['Jan 9'])
 print(temperatures['Jan 4'])
+
+'''--------'''
+
+###Count how many times each word appears in the text
+
+all_words = []
+with open("poem.txt", "r") as f:
+    for line in f:
+        line_string = line.strip()
+        individual_words = line_string.split(' ')
+        all_words = all_words + individual_words
+        
+all_words.remove("")
+
+word_count = {}
+
+for word in all_words:
+    print(word)
+    try:
+        if word_count[word]:
+            word_count[word] = word_count[word] + 1
+    except:
+        word_count[word] = 1
+
+print(word_count)
+
+# Better Implementation:
+# word_count = {}
+# with open("poem.txt","r") as f:
+#     for line in f:
+#         tokens = line.split(' ')
+#         for token in tokens:
+#             token=token.replace('\n','')
+#             if token in word_count:
+#                 word_count[token]+=1
+#             else:
+#                 word_count[token]=1
+
+# print(word_count)
+'''------------'''
+# Exercise 4 TODO
